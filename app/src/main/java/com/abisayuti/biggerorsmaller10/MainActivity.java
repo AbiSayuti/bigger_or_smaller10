7444ad094018afbc41c1843205beaf47ef8129e4
Abi SA <abisa3002@gmail.com>
2018-01-15 16:54:47 +0700

    Done

---
+++ app/src/main/java/com/abisayuti/biggerorsmaller10/MainActivity.java
@@ -0,0 +1,47 @@
+package com.abisayuti.biggerorsmaller10;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etinput;
+    Button btnHitung;
+    TextView txtHasil;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etinput = (EditText)findViewById(R.id.etinput);
+        btnHitung = (Button) findViewById(R.id.btnsumbit);
+        txtHasil = (TextView) findViewById(R.id.txthasil);
+
+        btnHitung.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String nnomor = etinput.getText().toString();
+
+                int anomor = Integer.parseInt(nnomor);
+
+                if (nnomor.isEmpty()) {
+                    etinput.setError("tidak boleh kosong");
+
+                }else if (anomor >= 10) {
+                    txtHasil.setText("smaller than 10");
+                }else if(anomor <= 10) {
+                    txtHasil.setText("bigger than 10");
+                }
+
+
+            }
+        });
+
+    }
+}
